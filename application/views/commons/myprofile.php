8bbb01fd3df6a05f9f8a412e10740f59003b18a2
pujie <pw.prayitno@gmail.com>
2016-03-14 16:59:00 +0700

    remove unused feature

--- application/views/commons/myprofile.php
+++ application/views/commons/myprofile.php
@@ -127,7 +127,6 @@
     <!-- AdminLTE App -->
     <script src="<?php echo baseurl()?>assets/padi/dist/js/app.min.js" type="text/javascript"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo baseurl()?>assets/padi/dist/js/demo.js" type="text/javascript"></script>
 	<script type="text/javascript" src="<?php echo baseurl();?>assets/ajaxupload.3.5.js"></script>
     <script type="text/javascript">
 		(function($){
